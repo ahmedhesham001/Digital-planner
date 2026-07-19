3f30cce9a90595096d2af9226746dcc70fde551e
ahmedhesham001 <ahmed.106300@cs.mti.edu.eg>
2026-07-19 12:00:23 -0400

    setup tailwind v4 design system, colors, and font

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@fontsource/geist-sans/400.css';
+import '@fontsource/geist-sans/500.css';
+import '@fontsource/geist-sans/600.css';
+import '@fontsource/geist-sans/700.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
