801398ba3b217300285543e68dc169e38c919f50
rawan907 <rawannmo73@gmail.com>
2026-08-01 04:26:18 +0300

    Implement login page UI

--- apps/web/src/pages/Login.jsx
+++ apps/web/src/pages/Login.jsx
@@ -1,28 +1,62 @@
 import "./Login.css"
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import icon from "../assets/Logos.svg"
 export default function Login() {
-    return (
-    <div className="login-page">
-    <div className="card">
-        <h1>Log in</h1>
-        <p>Log in to your account</p>
-        <div className="inputs">
-        <div>
-            <label>Email:</label>
-        <input type="email" placeholder="Enter your email address"/>
+return (
+    
+<div className="relative min-h-screen bg-[#0B0B0B] login-page p-5 flex flex-col items-center  justify-center">
+<div className="absolute w-160 h-160 rounded-full bg-[#A37108]/20 blur-[100px] top-5  left-160"></div>
+<div className="card relative z-10 bg-[#17130D] rounded-4 p-8 flex flex-col gap-5 w-105 ">
+    <div className="flex justify-center">
+        <img
+            src={icon}
+            alt="Logo"
+            className="w-[95px] h-[43px]"
+        />
         </div>
-        <div>
-            <label>Password:</label>
-<input type="password" placeholder="Enter your password"/>
-        </div>
-        </div>
-        <div><p><button>Login</button> With</p></div>
-        <div className="social">
-            <button>Facebook</button>
-            <button>Google</button>
-            <button>Apple</button>
-        </div>
-        <div><p> Don't have an account? <button> Sign Up</button></p></div>
+
+<hr className="border-white"></hr>
+
+<div >
+    <div >
+<label className="text-white">E-mail:</label>
     </div>
+<input className="w-full h-10 rounded-md border border-zinc-500 bg-transparent px-3 text-white placeholder:text-zinc-500" type="email" placeholder="rawan@gmail.com"/>
+</div>
+<div>
+<div>
+<label className="text-white">Password:</label>
     </div>
-    );
+<input className="w-full h-10 rounded-md border border-zinc-500 bg-transparent px-3 text-white placeholder:text-zinc-500" type="password" placeholder="**********"/>
+</div>
+<div>
+<button className="w-full h-10 rounded-md bg-[#EFA102] text-white font-semibold">Login</button>
+</div>
+<div className="flex items-center gap-3">
+          <hr className="flex-1 border-white" />
+          <span className="text-white">or</span>
+          <hr className="flex-1 border-white" />
+        </div>
+
+<div>
+<button className="w-full h-10 rounded-md border border-zinc-500 text-white hover:border-[#EFA102] transition">
+     <i className="fa-brands fa-google text-xl"></i>
+    <span> Login with Google</span>
+
+    </button>
+</div>
+<div>
+<button className="w-full h-10 rounded-md border border-zinc-500 text-white hover:border-[#EFA102] transition">
+     <i className="fa-brands fa-apple text-xl"></i>
+    <span> Login with Apple</span>
+    </button>
+</div>
+
+</div>
+</div>
+    
+
+
+
+);
 }
